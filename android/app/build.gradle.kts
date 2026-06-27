614e3483853515666fc3ea2347a292474122995e
hegazy <ahmedrady03@gmail.com>
2026-06-27 04:31:54 +0300

    added sharedpref

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.rahal"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
